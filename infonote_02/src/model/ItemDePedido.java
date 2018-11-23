bc042fd89f8d8a87423e1699401ffd5af06b5593
CarlosDaniel19 <c_daniel8@hotmail.com>
2018-11-23 20:54:55 -0200

    infonote_02

---
+++ infonote_02/src/model/ItemDePedido.java
@@ -0,0 +1,30 @@
+package model;
+
+public class ItemDePedido {
+	@Override
+	public String toString() {
+		return "ItemDePedido [qtde=" + qtde + ", subtotal=" + subtotal + ", getQtde()=" + getQtde() + ", getSubtotal()="
+				+ getSubtotal() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+
+	public int qtde;
+	public double subtotal;
+
+	public int getQtde() {
+		return qtde;
+	}
+
+	public void setQtde(int qtde) {
+		this.qtde = qtde;
+	}
+
+	public double getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+}
