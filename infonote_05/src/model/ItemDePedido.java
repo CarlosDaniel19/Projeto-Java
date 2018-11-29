3b989774ba349e18b2e7170e4ad0e0091ec4613a
CarlosDaniel19 <c_daniel8@hotmail.com>
2018-11-29 21:34:26 -0200

    arquivo

---
+++ infonote_05/src/model/ItemDePedido.java
@@ -0,0 +1,55 @@
+package model;
+
+public class ItemDePedido {
+	@Override
+	public String toString() {
+		return "ItemDePedido [qtde=" + qtde + ", subtotal=" + subtotal + ", getQtde()=" + getQtde() + ", getSubtotal()="
+				+ getSubtotal() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+
+	private int qtde;
+	private double subtotal;
+	private Notebook notebook;
+
+	public int getQtde() {
+		return qtde;
+	}
+
+	public void setQtde(int qtde) {
+		this.qtde = qtde;
+	}
+
+	public double getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public ItemDePedido() {
+		super();
+	}
+
+	public ItemDePedido(int qtde, double subtotal, Notebook aux) {
+		super();
+		this.qtde = qtde;
+		this.subtotal = subtotal;
+	}
+
+	public Notebook getNotebook() {
+		return notebook;
+	}
+
+	public void mostrar() {
+		System.out.println("\n\nItemDePedido:\n");
+		System.out.println("Quantidade: " + this.qtde);
+		System.out.println("Subtotal: " + this.subtotal);
+	}
+
+	public ItemDePedido(Notebook notebook) {
+		super();
+		this.notebook = notebook;
+	}
+}
