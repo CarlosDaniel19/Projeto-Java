1626902e7eb8c09656d444403a5a5902301fc634
CarlosDaniel19 <c_daniel8@hotmail.com>
2018-12-05 21:11:11 -0200

    arquivo

---
+++ infonote_08/src/model/Funcionario.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Funcionario extends Usuario implements IUsuario {
+
+	private String matricula;
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public Funcionario() {
+		super();
+	}
+
+	public Funcionario(String login, String senha, int tipo, String matricula) {
+		super(login, senha, tipo);
+		this.matricula = matricula;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionario [matricula=" + matricula + "]";
+	}
+
+	@Override
+	public boolean validarLogin(String login, String senha) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
